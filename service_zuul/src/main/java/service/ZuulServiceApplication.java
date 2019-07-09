7d98f6fb36d5e6e848ab331e369e97626e48415b
chengfengpiaopiao <13574845807@163.com>
2019-07-09 23:03:21 +0800

    Zuul

---
+++ service_zuul/src/main/java/service/ZuulServiceApplication.java
@@ -0,0 +1,17 @@
+package service;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * <pre>
+ *     Created by 2019/7/9 @author Gracie
+ *     e-mail : 13574845807@163.com
+ *     des :
+ *     version:
+ * </pre>
+ */
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulServiceApplication {
+}
